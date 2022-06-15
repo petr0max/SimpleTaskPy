f268417ad19b9dc3d12f79b13c208118ff56cb65
petr0max <mardinwijaya46@gmail.com>
2022-06-15 09:49:47 +0700

    initial commit

---
+++ task2/SearchMinMax.py
@@ -0,0 +1,5 @@
+searchValue = [2, 20, 25, -10, 78, 4, 0, -29, 21]
+
+print(f"Pencarian nilai max min pada data list {searchValue}")
+print(f"Nilai maksimum adalah : {max(searchValue)}")
+print(f"Nilai minimum adalah : {min(searchValue)}")
