9c1c920aeca55d4fb28941e498d7c059fc25446b
petr0max <mardinwijaya46@gmail.com>
2022-06-16 12:38:03 +0700

    initial commit

--- task1/fibonacciTest.py
+++ task1/fibonacciTest.py
@@ -10,7 +10,7 @@ def fibonacciGenerate():
     else:
         fiboNumbs = [0, 1]
         for i in range(2, inputGenerate):
-            fiboNumbs.append(fiboNumbs[i-2] + fiboNumbs[i - 1])
+            fiboNumbs.append(fiboNumbs[i - 2] + fiboNumbs[i - 1])
     
     soutfibo = [str(item) for item in fiboNumbs]
     print(" " . join(soutfibo))
